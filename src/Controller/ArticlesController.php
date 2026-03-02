235bc2b373da9a29ff0ed177e342aadf1fa3900f
Yesmine Boukrayaa <bkyesmina053@gmail.com>
2026-03-02 21:41:28 +0100

    feat: Formulaires Symfony avec ArticleType, validation et messages flash

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -3,30 +3,16 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ArticlesController extends AbstractController
 {
-    #[Route('/articles/nouveau', name: 'app_article_nouveau')]
-    public function nouveau(EntityManagerInterface $em): Response
-    {
-        $article = new Article();
-        $article->setTitre('Mon premier article');
-        $article->setContenu('Ceci est le contenu de mon premier article créé avec Doctrine.');
-        $article->setAuteur('Étudiant');
-        $article->setDateCreation(new \DateTime());
-        $article->setPublie(true);
-
-        $em->persist($article);
-        $em->flush();
-
-        return new Response("Article créé avec l'id : " . $article->getId());
-    }
-
     #[Route('/articles', name: 'app_articles')]
     public function index(ArticleRepository $articleRepository): Response
     {
@@ -36,11 +22,38 @@ final class ArticlesController extends AbstractController
             'articles' => $articles,
         ]);
     }
+
     #[Route('/articles/{id}', name: 'app_article_detail', requirements: ['id' => '\d+'])]
     public function detail(Article $article): Response
     {
         return $this->render('articles/detail.html.twig', [
             'article' => $article,
+        ]);
+    }
+
+    #[Route('/articles/nouveau', name: 'app_article_nouveau')]
+    public function nouveau(Request $request, EntityManagerInterface $em): Response
+    {
+        $article = new Article();
+
+        // Création du formulaire
+        $form = $this->createForm(ArticleType::class, $article);
+
+        // Traitement de la requête
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($article);
+            $em->flush();
+
+            $this->addFlash('success', 'Article créé avec succès !');
+
+            return $this->redirectToRoute('app_articles');
+        }
+
+        return $this->render('articles/nouveau.html.twig', [
+            'formulaire' => $form->createView(),
         ]);
     }
 }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\ArticleRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ArticleRepository::class)]
 class Article
@@ -15,19 +16,44 @@ class Article
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'Le titre ne peut pas être vide.')]
+    #[Assert\Length(
+        min: 5,
+        max: 255,
+        minMessage: 'Le titre doit contenir au moins {{ limit }} caractères.',
+        maxMessage: 'Le titre ne peut pas dépasser {{ limit }} caractères.'
+    )]
+    #[Assert\Regex(
+    pattern: '/^[a-zA-ZÀ-ÿ\s\-]+$/',
+    message: 'Le nom de l\'auteur ne peut contenir que des lettres, espaces et tirets.'
+    )]
     private ?string $titre = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank(message: 'Le contenu ne peut pas être vide.')]
+    #[Assert\Length(
+        min: 20,
+        minMessage: 'Le contenu doit contenir au moins {{ limit }} caractères.'
+    )]
     private ?string $contenu = null;
 
     #[ORM\Column(length: 100)]
+    #[Assert\NotBlank(message: 'L\'auteur est obligatoire.')]
+    #[Assert\Length(
+        min: 2,
+        max: 100,
+        minMessage: 'Le nom de l\'auteur doit contenir au moins {{ limit }} caractères.'
+    )]
     private ?string $auteur = null;
 
-    #[ORM\Column]
-    private ?\DateTime $dateCreation = null;
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    #[Assert\NotNull(message: 'La date de création est obligatoire.')]
+    private ?\DateTimeInterface $dateCreation = null;
 
-    #[ORM\Column]
-    private ?bool $publie = null;
+    #[ORM\Column(type: Types::BOOLEAN)]
+    private ?bool $publie = false;
+
+    // ================= GETTERS & SETTERS =================
 
     public function getId(): ?int
     {
@@ -42,7 +68,6 @@ class Article
     public function setTitre(string $titre): static
     {
         $this->titre = $titre;
-
         return $this;
     }
 
@@ -54,7 +79,6 @@ class Article
     public function setContenu(string $contenu): static
     {
         $this->contenu = $contenu;
-
         return $this;
     }
 
@@ -66,19 +90,17 @@ class Article
     public function setAuteur(string $auteur): static
     {
         $this->auteur = $auteur;
-
         return $this;
     }
 
-    public function getDateCreation(): ?\DateTime
+    public function getDateCreation(): ?\DateTimeInterface
     {
         return $this->dateCreation;
     }
 
-    public function setDateCreation(\DateTime $dateCreation): static
+    public function setDateCreation(\DateTimeInterface $dateCreation): static
     {
         $this->dateCreation = $dateCreation;
-
         return $this;
     }
 
@@ -90,7 +112,6 @@ class Article
     public function setPublie(bool $publie): static
     {
         $this->publie = $publie;
-
         return $this;
     }
 }
