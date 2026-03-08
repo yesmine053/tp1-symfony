440b0f1bbbfe4fc1796a38e75a621b193c689844
Yesmine Boukrayaa <bkyesmina053@gmail.com>
2026-03-08 23:13:40 +0100

    feat: CRUD complet articles avec modification, suppression et protection CSRF

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -43,4 +43,37 @@ final class ArticlesController extends AbstractController
             'article' => $article,
         ]);
     }
+    #[Route('/articles/{id}/modifier', name: 'app_article_modifier', requirements: ['id' => '\d+'])]
+    public function modifier(Article $article, Request $request, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush(); // Pas besoin de persist() car l'entité est déjà gérée par Doctrine
+
+            $this->addFlash('success', 'Article modifié avec succès !');
+            return $this->redirectToRoute('app_article_detail', ['id' => $article->getId()]);
+        }
+
+        return $this->render('articles/modifier.html.twig', [
+            'formulaire' => $form,
+            'article' => $article,
+        ]);
+    }
+    #[Route('/articles/{id}/supprimer', name: 'app_article_supprimer', requirements: ['id' => '\d+'], methods: ['POST'])]
+    public function supprimer(Article $article, Request $request, EntityManagerInterface $em): Response
+    {
+        // Vérification du token CSRF pour la sécurité
+        if ($this->isCsrfTokenValid('supprimer_' . $article->getId(), $request->request->get('_token'))) {
+            $em->remove($article);
+            $em->flush();
+
+            $this->addFlash('success', 'Article supprimé avec succès.');
+        } else {
+            $this->addFlash('danger', 'Token CSRF invalide. Suppression annulée.');
+        }
+
+        return $this->redirectToRoute('app_articles');
+    }
 }
